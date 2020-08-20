8a4654c319ec2e7994bc2948ed363615461b0442
Mihail Kovachev <bionic404.notfound@protonmail.com>
2020-08-20 14:55:34 +0300

    Added player card drawing functionality (not tested)

--- WarPredict0r/Player.cpp
+++ WarPredict0r/Player.cpp
@@ -1,1 +1,26 @@
 #include "Player.h"
+
+Player::Player()
+{
+	
+}
+
+bool Player::DrawNext(std::vector<Card>& drawedCards, unsigned int amount)
+{
+	if (deck.size() >= amount) return false;
+
+	if (amount <= 3 && amount >= 1)
+	{
+		for (unsigned int i = 1; i <= amount; ++i)
+		{
+			drawedCards.emplace_back(deck.front());
+			deck.pop();
+		}
+	}
+	return true;
+}
+
+void Player::AppendToDeck(Card card)
+{
+	deck.push(card);
+}
--- WarPredict0r/Player.h
+++ WarPredict0r/Player.h
@@ -1,6 +1,36 @@
 #pragma once
 
+#include <queue>
+#include <vector>
+
 class Player
 {
+public:
+	enum class Card
+	{
+		Two = 2,
+		Three = 3,
+		Four = 4,
+		Five = 5,
+		Six = 6,
+		Seven = 7,
+		Eight = 8,
+		Nine = 9,
+		Ten = 10,
+		J = 11,
+		Q = 12,
+		K = 13,
+		A = 14
+	};
+
+public:
+	Player();
+
+	// Returns true if the player had enough cards to draw the amount from
+	bool DrawNext(std::vector<Card>& drawedCards, unsigned int amount = 1);
+	void AppendToDeck(Card card);
+
+private:
+	std::queue<Card> deck;
 };
 
